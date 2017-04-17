4a13f23bd72ab30b84c5c3d5278029435e60812d
Dominic Brown and Grady L Shelton <grady@>
2017-04-17 11:59:19 -0700

    add code for spec to replace t with 7

--- lib/leet.rb
+++ lib/leet.rb
@@ -9,11 +9,13 @@ class String
         leet_array.push('3')
       elsif letter.downcase.include?("o")
         leet_array.push('0')
+      elsif letter.downcase.include?("t")
+        leet_array.push('7')
       else
         leet_array.push(letter)
       end
     end
-    
+
     leet_array.join
   end
 end
--- spec/leet_spec.rb
+++ spec/leet_spec.rb
@@ -9,13 +9,18 @@ describe('String#leet') do
   end
 
   it("output '3's in place of all 'e's and 'E's ") do
-    myStr2 = "leetspeak"
-    expect(myStr2.leet).to(eq('l33tsp3ak'))
+    myStr2 = "Bruce Lee"
+    expect(myStr2.leet).to(eq('Bruc3 L33'))
   end
 
   it("output '0's in place of all 'o's and 'O's ") do
+    myStr2 = "I like books"
+    expect(myStr2.leet).to(eq("I lik3 b00ks"))
+  end
+
+  it("output '7's in place of all 't's and 'T's ") do
     myStr2 = "leetspeak doesn't make sense"
-    expect(myStr2.leet).to(eq("l33tsp3ak d03sn't mak3 s3ns3"))
+    expect(myStr2.leet).to(eq("l337sp3ak d03sn'7 mak3 s3ns3"))
   end
 
 end
