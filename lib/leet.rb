0e9316d04e79ede0d3e871ecaff7b3145c19c5f8
Dominic Brown and Grady L Shelton <dominic@>
2017-04-17 11:37:59 -0700

    add code for 2nd spec

--- lib/leet.rb
+++ lib/leet.rb
@@ -1,6 +1,15 @@
 
 class String
   define_method(:leet) do
-    self
+    leet_array = []
+    user_array = self.split("")
+    user_array.each do |letter|
+      if letter.include?("e")
+        leet_array.push('3')
+      else
+        leet_array.push(letter)
+      end
+    end
+    leet_array.join
   end
 end
--- spec/leet_spec.rb
+++ spec/leet_spec.rb
@@ -2,8 +2,15 @@ require('rspec')
 require('leet')
 
 describe('String#leet') do
+
   it('output the string the user inputs') do
-    myStr = "leetspeak"
-    expect(myStr.leet).to(eq('leetspeak'))
+    myStr1 = "bdd unfun"
+    expect(myStr1.leet).to(eq('bdd unfun'))
   end
+
+  it('output the string the user inputs') do
+    myStr2 = "leetspeak"
+    expect(myStr2.leet).to(eq('l33tsp3ak'))
+  end
+
 end
