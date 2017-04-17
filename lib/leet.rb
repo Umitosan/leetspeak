24ab226c2c4a4405b9c329bc9493d7d7a36c2558
Dominic Brown and Grady L Shelton <dominic@>
2017-04-17 11:52:10 -0700

    add code for spec 3

--- lib/leet.rb
+++ lib/leet.rb
@@ -3,13 +3,17 @@ class String
   define_method(:leet) do
     leet_array = []
     user_array = self.split("")
+
     user_array.each do |letter|
-      if letter.include?("e")
+      if letter.downcase.include?("e")
         leet_array.push('3')
+      elsif letter.downcase.include?("o")
+        leet_array.push('0')
       else
         leet_array.push(letter)
       end
     end
+    
     leet_array.join
   end
 end
--- spec/leet_spec.rb
+++ spec/leet_spec.rb
@@ -3,14 +3,19 @@ require('leet')
 
 describe('String#leet') do
 
-  it('output the string the user inputs') do
+  it("output the string the user inputs") do
     myStr1 = "bdd unfun"
     expect(myStr1.leet).to(eq('bdd unfun'))
   end
 
-  it('output the string the user inputs') do
+  it("output '3's in place of all 'e's and 'E's ") do
     myStr2 = "leetspeak"
     expect(myStr2.leet).to(eq('l33tsp3ak'))
+  end
+
+  it("output '0's in place of all 'o's and 'O's ") do
+    myStr2 = "leetspeak doesn't make sense"
+    expect(myStr2.leet).to(eq("l33tsp3ak d03sn't mak3 s3ns3"))
   end
 
 end
